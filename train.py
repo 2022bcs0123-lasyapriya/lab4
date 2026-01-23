b72bd5f5a43874b356560958d72024a50b7ad0f8
2022bcs0123-lasyapriya <lasyabagadi@gmail.com>
2026-01-23 13:01:54 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,17 +5,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier  # or Regressor
 from sklearn.metrics import f1_score, accuracy_score
 
-# Option 1: use local file
-df = pd.read_csv("winequality-red.csv", sep=";")   # or winequality-white.csv
-# or Option 2: download from UCI
-# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-# df = pd.read_csv(url, sep=";")
+df = pd.read_csv("winequality.csv", sep=";")   
+
 
 X = df.drop("quality", axis=1)
 y = df["quality"]
 
-# Optional: make it binary classification (common in tutorials)
-# y = (y >= 6).astype(int)   # good vs bad
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -24,6 +19,5 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
-# or print("F1:", f1_score(y_test, y_pred, average="weighted"))
 
 joblib.dump(model, "model/model.pkl")
