fa24b5de4baffdbc9cca141d5019d2701f958f24
2022bcs0123-lasyapriya <lasyabagadi@gmail.com>
2026-02-27 18:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import numpy as np
 
 app = FastAPI()
 
-# Load trained model
 model = joblib.load("model/model.pkl")
 
-# Wine dataset has 13 features — fixed order
 FEATURE_ORDER = [
-    "fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-    "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density",
-    "pH", "sulphates", "alcohol"
+    "fixed acidity", "volatile acidity", "citric acid",
+    "residual sugar", "chlorides", "free sulfur dioxide",
+    "total sulfur dioxide", "density", "pH",
+    "sulphates", "alcohol"
 ]
 
 @app.get("/health")
@@ -21,10 +20,8 @@ def health():
 @app.post("/predict")
 def predict(data: dict):
     try:
-        # Convert input dict to ordered feature list
         features = [data[f] for f in FEATURE_ORDER]
         features = np.array(features).reshape(1, -1)
-
         prediction = model.predict(features)[0]
 
         return {
@@ -32,8 +29,5 @@ def predict(data: dict):
             "roll_no": "2022BCS0123",
             "wine_quality": int(prediction)
         }
-
     except KeyError as e:
-        return {
-            "error": f"Missing feature: {str(e)}"
-        }
+        return {"error": f"Missing feature: {str(e)}"}
